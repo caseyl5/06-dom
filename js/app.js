d0f848ae22aabb42e0b100673c6babccbd377037
Joel Ross <joelross@uw.edu>
2016-06-28 19:15:50 -0700

    End of lecture code

--- js/app.js
+++ js/app.js
@@ -1,3 +1,61 @@
 'use strict';
 
-/* your script goes here */
+window.onload = function () {
+
+    /* your script goes here */
+
+    // console.log("Wait for it...");
+    // window.setTimeout(function() {
+    //     console.log("Toast is done!!");
+    // }, 5000);
+
+    // function doRepeatedly() { console.log("Are we there yet?"); }
+
+    // //run every 1 second
+    // var timerId = window.setInterval(doRepeatedly, 1000);
+
+    function blink() {
+        var header = document.querySelector('header');
+        header.classList.toggle('red');
+        // header.classList.toggle('white');
+    }
+
+    //window.setInterval(blink, 500);
+
+    var img = document.querySelector('img');
+    var clickCount = 0;
+
+    function handleClick(event) {
+        console.log('clicky clicky');
+
+        var elem = event.target;
+        console.log(elem);
+
+        clickCount++;
+        if (clickCount > 10) {
+            elem.setAttribute('src', 'img/husky.jpg');
+        }
+        else {
+            console.log(clickCount);
+        }
+    }
+
+
+    img.addEventListener('click', handleClick);
+
+    //Select the circle, and when it's clicked, 
+    //turn it red!
+    var circle = document.querySelector('circle');
+    circle.addEventListener('click', function (event) {
+        circle.setAttribute('fill', 'red');
+    })
+
+
+}
+
+
+
+
+
+
+
--- js/warmup.js
+++ js/warmup.js
@@ -3,24 +3,32 @@
 //be sure and test your function! Here is a string you can use...
 var testString = "99 luftballons / auf ihrem weg zum horizont";
 
-
 //Convert the test string into an array of words. Log out the results
-
+var words = testString.split(' ');
+console.log(words);
 
 //Write a function 'capitalize' that takes in a String and 
 //returns a new String that has the first letter in upper case
 //Reference: http://www.w3schools.com/jsref/jsref_obj_string.asp
 //be sure and test your function!
-
-
+var capitalize = function(string) {
+    var firstLetter = string.charAt(0).toUpperCase();
+    var rest = string.slice(1);
+    return firstLetter + rest;
+}
 
 //Now pass each value in your words array to the capitalize function by using the 
 //.map() function, and save the result to a new variable
 
-
+var capWords = words.map(capitalize);
+console.log(capWords);
 
 //Using .map() with an ANONYMOUS FUNCTION, add the text 
 //"Give me a " to each element in your capitalized array. 
 //Save the result to a new var (e.g., "cheer") and log it out
 
+var cheer = capWords.map(function(string) {
+    return "Give me a "+ string;
+});
 
+console.log(cheer);
